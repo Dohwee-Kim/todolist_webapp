21e10115f3a2b928d1a6fb307cf6ceeb7c2eeea2
Leo <cloudkim2024@gmail.com>
2021-11-27 17:03:11 -0500

    index.html added for home loading

--- server.js
+++ server.js
@@ -12,3 +12,10 @@ app.get('/beauty', function(req, res) {
     res.send('Pet shopping page')
     console.log('accessed to the Pet');
 });
+
+//  슬래시 하나만 쓰면 홈 
+app.get('/', function(req, res) {
+    //res.send('Home')
+    res.sendFile(__dirname + '/index.html');
+    console.log('Home page access ');
+});
